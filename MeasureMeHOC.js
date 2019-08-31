4ecda0c0d4b0c304bb0ab44d757aea85261dd454
Pawan <pawan.kumar1@myntra.com>
2019-08-31 16:24:04 +0530

    clean up a bit

--- MeasureMeHOC.js
+++ MeasureMeHOC.js
@@ -1,29 +1,33 @@
-'use strict';
-
 /**
 * @providesModule MeasureMeHOC
 **/
-import React, {Component} from 'react';
-import {View} from 'react-native';
+import React, { Component } from 'react'
+import { View } from 'react-native'
 
-module.exports = function measureMeHOC(ComposedComponent) {
-  return class extends Component {
-    constructor(props){
-      super(props);
-      this.state = {width:0, height:0, initialRender:false};
-      this.measure = this.measure.bind(this);
-    }
+export default ComposedComponent =>
+    class extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {}
+            this.measure = ({
+                nativeEvent: {
+                    layout: {
+                        width = 0,
+                        height = 0,
+                    } = {}
+                } = {}
+            }) => this.setState({ width, height, initialRender: true })
+        }
 
-    measure({nativeEvent : {layout : {width = 0, height = 0} = {}} = {}}){
-      this.setState({width, height, initialRender:true});
+        render() {
+            return (
+              this.state.initialRender
+                ? <ComposedComponent
+                    { ...this.props }
+                    { ...this.state }
+                    onLayout={ this.measure }
+                />
+                : <View style={this.props.style} onLayout={this.measure} />
+            );
+        }
     }
-
-    render(){
-      return (
-        this.state.initialRender ?
-          <ComposedComponent {...this.props} {...this.state} onLayout={this.measure}/> :
-          <View style={this.props.style} onLayout={this.measure} />
-      );
-    }
-  }
-}
